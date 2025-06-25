4a9f79c732c374a809d2072d4409a02344bfbcb6
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2025-06-25 16:10:35 -0300

    LISTAS

--- 2025-06-25 - LISTAS/exemplo_02.py
+++ 2025-06-25 - LISTAS/exemplo_02.py
@@ -6,6 +6,6 @@
       2) Gere uma lista com N valores inteiros aleatórios entre 0 e 1.000 (inclusive)
          sem repetições;
 
-      3) A partir da lista gerada, gere uma segunda uma lista com os fatoriais dos 
-         valores da lista anterior;
+      3) A partir da lista gerada, gere uma segunda uma lista com as raízes quadradas 
+         dos valores da lista anterior;
 '''
