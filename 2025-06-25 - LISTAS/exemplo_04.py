2d2bbc04ef39b195e2dabb155e78931beddfff9b
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2025-06-25 14:00:01 -0300

    LISTAS

---
+++ 2025-06-25 - LISTAS/exemplo_04.py
@@ -0,0 +1,24 @@
+'''
+   Dada a lista lstNomes (no início do código), faça um programa que:
+   
+      1) Gere mais duas listas (lstNotas_1 e lstNotas_2), cada uma com N notas aleatórias
+         entre 0 e 100 (inclsuive). Cada uma dessas listas deverá ter exatamente notas
+         para cada um dos nomes em lstNomes;
+
+      2) Gere uma lista (lstBoletim), onde cada posição será uma sub-lista contendo um dos
+         dos nomes de lstNomes e suas respectivas notas (lstNotas_1 e lstNotas_2);
+
+      3) Após lstBoletim ser gerada, adicione em cada posição mais duas informações:
+
+         a) A média das notas (utilize como referência o cálculo de média do IFRN);
+
+         b) A situação do aluno (Aprovado, Prova Final ou Reprovado) seguindo os 
+            critérios do IFRN;
+'''
+
+lstNomes = ['Scooby-Doo'       , 'Fred Flintstone', 'Zé Colmeia' , 'Dom Pixote'     , 
+            'Muttley'          , 'Binicão'        , 'Tutubarão'  , 'Capitão Caverna', 
+            'Formiga Atômica'  , 'Jonny Quest'    , 'Space Ghost', 'Manda-Chuva'    , 
+            'Barney Rubble'    , 'Salsicha'       , 'Falcão Azul', 'Babatinha'      , 
+            'Penélope Charmosa', 'Pepe Legal'     , 'Catatau'    , 'Dick Vigarista' ]
+
